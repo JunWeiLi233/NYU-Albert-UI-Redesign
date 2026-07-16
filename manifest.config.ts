b77fadd1cf40c77a64a4bb6faf97d6d916ea44fc
JunWeiLi233 <mcpejunwei@gmail.com>
2026-07-17 00:43:10 +0800

    Release Better Albert v0.3.8

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Better Albert",
-  version: "0.3.7",
+  version: "0.3.8",
   description:
     "A local-first, NYU-aligned interface enhancement for NYU Albert.",
   permissions: ["storage"],
