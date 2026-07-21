20859d91b9d20773208f159a660e7f6f483c5514
Junwei <70492516+JunWeiLi233@users.noreply.github.com>
2026-07-21 19:52:24 +0800

    Add mobile task finder and release v0.4.9 (#23)

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Better Albert",
-  version: "0.4.8",
+  version: "0.4.9",
   description:
     "A local-first, NYU-aligned interface enhancement for NYU Albert.",
   permissions: ["storage"],
